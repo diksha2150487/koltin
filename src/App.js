64bb890ff7ec9363c4ee85b7efd03704e48b0e48
diksha2150487 <130853102+diksha2150487@users.noreply.github.com>
2023-05-25 18:19:51 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { AiOutlineArrowRight } from "react-icons/ai";
-import img1 from "./image/Group 1000006006.png";
+
 function App() {
   return (
     <div>
       <div className="paper1">
         <div className="leftpaper1">
-          {/* <h3>Expert BigCommerce Consulting</h3> */}
           <h2>Kotlin App Development </h2>
           <h3>Company</h3>
           <button className="team">
@@ -15,10 +14,8 @@ function App() {
             <AiOutlineArrowRight id="fArrow" />
           </button>
         </div>
-        {/* <div className="rightpaper1"> */}
+
         <div className="circle"></div>
-        {/* <img src={img1} alt="img" width={300} className="rightimg1" /> */}
-        {/* </div> */}
       </div>
     </div>
   );
